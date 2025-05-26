02d611c72036cfb1166142ce6489ce79abb0b3ca
pyun seona <vustjsdk1@naver.com>
2025-05-26 15:58:18 +0900

    Feat#5 최종 수정

--- demo/src/main/java/com/example/demo/repository/ReviewRepository/ReviewRepository.java
+++ demo/src/main/java/com/example/demo/repository/ReviewRepository/ReviewRepository.java
@@ -10,6 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findAllByStore(Store store, PageRequest pageRequest);
-    // 로그인한 회원(=member)이 쓴 리뷰만 페이징 조회
-    Page<Review> findByMemberId(Long memberId, Pageable pageable);
+
 }
